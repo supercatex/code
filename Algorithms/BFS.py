877594d78c63d269ff8f0a13062e8b3e702b469f
Kinda <lamkinun@gmail.com>
2018-08-21 11:32:06 +0800

    Add files via upload

---
+++ Algorithms/BFS.py
@@ -0,0 +1,42 @@
+from queue import Queue
+
+if __name__ == '__main__':
+    # Tree contains 12 nodes.
+    tree_names = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L']
+    T = []
+    for i in range(len(tree_names)):
+        T.append(i)
+    
+    # Relationship between nodes.
+    R = [
+        [1, 2, 3],      # 0     A
+        [4, 5],         # 1     B
+        [7],            # 2     C
+        [8, 9],         # 3     D
+        [10],           # 4     E
+        [],             # 5     F
+        [],             # 6     G
+        [6],            # 7     H
+        [],             # 8     I
+        [11],           # 9     J
+        [],             # 10    K
+        []              # 11    L
+        ]
+    
+    # Using data structure Queue in BFS.
+    Q = Queue()
+    
+    # Adding the first node into the Queue.
+    Q.put(T[0])
+    
+    # Loop until Queue is empty.
+    while Q.empty() == False:
+        # Get one node from the Queue.
+        node = Q.get()
+        
+        # do something here...
+        print(tree_names[node])
+        
+        # Put all child nodes into the Queue.
+        for i in R[node]:
+            Q.put(i)
