ea387d2e55b8edc967993e7853c214740fe680c2
Kinda <lamkinun@gmail.com>
2018-08-27 01:15:30 +0800

    Update setup.sh

--- ros/setup.sh
+++ ros/setup.sh
@@ -46,5 +46,7 @@ sh -c "echo \"export EDITOR='nano -w'\" >> ~/.bashrc"
 
 source $HOME/.bashrc
 
+sudo apt-get install ros-kinetic-joy ros-kinetic-teleop-twist-joy ros-kinetic-teleop-twist-keyboard ros-kinetic-laser-proc ros-kinetic-rgbd-launch ros-kinetic-depthimage-to-laserscan ros-kinetic-rosserial-arduino ros-kinetic-rosserial-python ros-kinetic-rosserial-server ros-kinetic-rosserial-client ros-kinetic-rosserial-msgs ros-kinetic-amcl ros-kinetic-map-server ros-kinetic-move-base ros-kinetic-urdf ros-kinetic-xacro ros-kinetic-compressed-image-transport ros-kinetic-rqt-image-view ros-kinetic-gmapping ros-kinetic-navigation ros-kinetic-interactive-markers
+
 echo "[Complete!!!]"
 exit 0
