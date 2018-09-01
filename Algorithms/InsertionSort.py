7424f53e33936d6ae7a6a688d1d7946f03811225
Kinda <lamkinun@gmail.com>
2018-09-01 11:43:39 +0800

    Update InsertionSort.py

--- Algorithms/InsertionSort.py
+++ Algorithms/InsertionSort.py
@@ -1,25 +1,25 @@
-if __name__ == '__main__':
-    # define a numeric list
-    S = [5, 3, 4, 7, 2, 8, 6, 9, 1]
-    
+def insertionSrot(n):
     # sorted count start from 1 to n
     c = 1
-    while c < len(S):
+    while c < len(n):
         
         # compare the sorted list and swap to the right space
         i = c
         while i > 0:
-            print(S, 'compare: S[%d] > S[%d]' % (i, i - 1))
+            print(n, 'compare: S[%d] > S[%d]' % (i, i - 1))
             
-            if S[i] < S[i - 1]:
-                t = S[i] 
-                S[i] = S[i - 1] 
-                S[i - 1] = t
+            if n[i] < n[i - 1]:
+                t = n[i] 
+                n[i] = n[i - 1] 
+                n[i - 1] = t
             
             i = i - 1
         
         # next iteration
         print()
         c = c + 1
-        
-    print(S)
+    
+    return n
+    
+if __name__ == '__main__':
+    print(insertionSrot([5, 3, 4, 7, 2, 8, 6, 9, 1]))
