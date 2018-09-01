c5432d401a0e6fffc2c60cd3553402d885c2f5f8
Kinda <lamkinun@gmail.com>
2018-09-01 11:45:30 +0800

    Update SelectionSort.py

--- Algorithms/SelectionSort.py
+++ Algorithms/SelectionSort.py
@@ -1,35 +1,35 @@
-if __name__ == '__main__':
-    # define a numeric list
-    S = [5, 9, 3, 1, 2, 8, 4, 7, 6]
-    
+def selectionSort(n):
     # sorted count from 0 to n - 1
     c = 0
-    while c < len(S) - 1:
+    while c < len(n) - 1:
         
         # default minimize index is the first one
         min_index = c
         
         # i start from the second one to the end of the list
         i = c + 1
-        while i < len(S):
+        while i < len(n):
             
-            print(S, 'the minimize data is %d' % S[min_index])
+            print(n, 'the minimize data is %d' % n[min_index])
             
             # update the minimize index
-            if S[i] < S[min_index]:
+            if n[i] < n[min_index]:
                 min_index = i 
             
             # next step
             i = i + 1
         
-        print(S, 'the final minimize data is %d\n' % S[min_index])
+        print(n, 'the final minimize data is %d\n' % n[min_index])
         
         # swap the minimize one next to the sorted numeric.
-        t = S[c]
-        S[c] = S[min_index]
-        S[min_index] = t 
+        t = n[c]
+        n[c] = n[min_index]
+        n[min_index] = t 
         
         # next iteration
         c = c + 1
     
-    print(S)
+    return n
+    
+if __name__ == '__main__':
+    print(selectionSort([5, 9, 3, 1, 2, 8, 4, 7, 6]))
