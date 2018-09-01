d7ced0bcc1c86ef1aa1ae81d6c733dea8f7a8201
Kinda <lamkinun@gmail.com>
2018-09-01 11:41:45 +0800

    Update BubbleSort.py

--- Algorithms/BubbleSort.py
+++ Algorithms/BubbleSort.py
@@ -1,28 +1,26 @@
-if __name__ == '__main__':
-    # define a numeric list
-    S = [5, 9, 3, 1, 2, 8, 4, 7, 6]
-    
+def bubbleSort(n):
     # sorted count from 0 to n - 1
     c = 0
-    while c < len(S) - 1:
+    while c < len(n) - 1:
         
         # i from the end of list to sorted count
-        i = len(S) - 1
+        i = len(n) - 1
         while i > c:
-            print(S, 'compare: S[%d] < S[%d]' % (i, i - 1))
+            print(n, 'compare: S[%d] < S[%d]' % (i, i - 1))
             
             # compare i & i - 1
-            if S[i] < S[i - 1]:
-                t = S[i]
-                S[i] = S[i - 1]
-                S[i - 1] = t
+            if n[i] < n[i - 1]:
+                t = n[i]
+                n[i] = n[i - 1]
+                n[i - 1] = t
             
             # next step
             i = i - 1
             
         # next iteration
         c = c + 1
-        print()
     
-    # final output
-    print(S)
+    return n
+
+if __name__ == '__main__':
+    print(bubbleSort([5, 9, 3, 1, 2, 8, 4, 7, 6]))
