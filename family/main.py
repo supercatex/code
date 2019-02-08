fbf5162b71c6f60114655a20ec782c836613d21d
gapry <gapry.mo@gmail.com>
2019-02-08 09:29:14 +0800

    [family] code refactoring

--- family/main.py
+++ family/main.py
@@ -1,110 +1,113 @@
 from problog.program import Term, Var, SimpleProgram
 from problog.engine import DefaultEngine
 
+import re
 
-def print_relationship(a, b, terms, program):
-    engine = DefaultEngine()
-    db = engine.prepare(program)
+# verify the entry from the database
+dbg_print = lambda xs: [print(k, v) for k, v in xs.items()]
 
-    print("[%s]是[%s]的 --> " % (b, a), end="")
-    for key in terms:
-        query_term = terms[key](a, b)
-        res = engine.query(db, query_term)
-        if bool(res):
-            print(terms[key], end=" ")
-    print("")
+# define a class to support read files and put the rows into dict()
+class file_manager:
+  def __init__(self, filename_):
+    self.filename = filename_
 
+  def get_dict(self):
+    xs = dict()
 
-t = dict()
-t["myself"] = Term("自己")
-t["husband"] = Term("丈夫")
-t["wife"] = Term("妻子")
-t["couple"] = Term("配偶")
-t["son"] = Term("兒子")
-t["daughter"] = Term("女兒")
-t["child"] = Term("孩子")
-t["parent"] = Term("父母")
-t["mother"] = Term("媽媽")
-t["father"] = Term("爸爸")
-t["sibling"] = Term("手足")
-t["grandfather"] = Term("祖父")
-t["grandmother"] = Term("祖母")
-t["grandparents"] = Term("祖父母")
-t["grandson"] = Term("孫兒子")
-t["granddaughter"] = Term("孫女兒")
-t["grandchildren"] = Term("孫子女")
-t["grandfather_"] = Term("外祖父")
-t["grandmother_"] = Term("外祖母")
-t["grandparents_"] = Term("外祖父母")
-t["grandson_"] = Term("外孫兒子")
-t["granddaughter_"] = Term("外孫女兒")
-t["grandchildren_"] = Term("外孫子女")
-t["son-wife"] = Term("媳婦")
-t["daughter-husband"] = Term("女婿")
-t["husband-mother"] = Term("婆婆")
-t["husband-father"] = Term("公公")
-t["wife-mother"] = Term("丈母娘")
-t["wife-father"] = Term("丈人")
+    def fetch_lines(fd):
+      for line in fd:
+        yield line
 
-X, Y, Z = map(Var, ["X", "Y", "Z"])
-sp = SimpleProgram()
-sp += t["myself"](X, Y) << t["myself"](X, Y)
-sp += t["husband"](X, Y) << t["wife"](Y, X)
-sp += t["wife"](X, Y) << t["husband"](Y, X)
-sp += t["couple"](X, Y) << (t["husband"](X, Y) | t["wife"](X, Y))
-sp += t["couple"](X, Y) << t["couple"](Y, X)
-sp += t["son"](X, Y) << (t["son"](X, Y) | (t["couple"](X, Z) & t["son"](Z, Y)))
-sp += t["daughter"](X, Y) << (t["daughter"](X, Y) | (t["couple"](X, Z) & t["daughter"](Z, Y)))
-sp += t["child"](X, Y) << (t["son"](X, Y) | t["daughter"](X, Y))
-sp += t["child"](X, Y) << (t["father"](Y, X) | t["mother"](Y, X))
-sp += t["child"](X, Y) << (t["couple"](X, Z) & t["child"](Z, Y))
-sp += t["parent"](X, Y) << t["child"](Y, X)
-sp += t["mother"](X, Y) << (t["wife"](Z, Y) & t["child"](Y, X))
-sp += t["father"](X, Y) << (t["husband"](Z, Y) & t["child"](Y, X))
-sp += t["sibling"](X, Y) << (t["parent"](X, Z) & t["parent"](Y, Z))
-sp += t["grandfather"](X, Y) << (t["father"](X, Z) & t["father"](Z, Y))
-sp += t["grandmother"](X, Y) << (t["father"](X, Z) & t["mother"](Z, Y))
-sp += t["grandparents"](X, Y) << (t["grandfather"](X, Y) | t["grandmother"](X, Y))
-sp += t["grandson"](X, Y) << (t["son"](X, Z) & t["son"](Z, Y))
-sp += t["granddaughter"](X, Y) << (t["son"](X, Z) & t["daughter"](Z, Y))
-sp += t["grandchildren"](X, Y) << (t["son"](X, Z) & t["child"](Z, Y))
-sp += t["grandfather_"](X, Y) << (t["mother"](X, Z) & t["father"](Z, Y))
-sp += t["grandmother_"](X, Y) << (t["mother"](X, Z) & t["mother"](Z, Y))
-sp += t["grandparents_"](X, Y) << (t["grandfather_"](X, Y) | t["grandmother_"](X, Y))
-sp += t["grandson_"](X, Y) << (t["daughter"](X, Z) & t["son"](Z, Y))
-sp += t["granddaughter_"](X, Y) << (t["daughter"](X, Z) & t["daughter"](Z, Y))
-sp += t["grandchildren_"](X, Y) << (t["daughter"](X, Z) & t["child"](Z, Y))
-sp += t["son-wife"](X, Y) << (t["husband"](Y, Z) & t["parent"](Z, X))
-sp += t["daughter-husband"](X, Y) << (t["wife"](Y, Z) & t["parent"](Z, X))
-sp += t["daughter-husband"](X, Y) << (t["wife"](Y, Z) & t["parent"](Z, X))
-sp += t["husband-mother"](X, Y) << (t["husband"](X, Z) & t["mother"](Z, Y))
-sp += t["husband-father"](X, Y) << (t["husband"](X, Z) & t["father"](Z, Y))
-sp += t["wife-mother"](X, Y) << (t["wife"](X, Z) & t["mother"](Z, Y))
-sp += t["wife-father"](X, Y) << (t["wife"](X, Z) & t["father"](Z, Y))
+    try:
+      fd = open(self.filename, "r")
+      rx = re.compile('([\n" "])')
+      for line in fetch_lines(fd):
+        data = rx.sub(r'', line).split("=")
+        xs[data[0]] = Term(data[1])
+      fd.close()
+      # dbg_print(xs)
+      return xs
+    except Exception as err:
+      print(err)
 
-n = dict()
-n["林爸爸"] = Term("林爸爸")
-n["林媽媽"] = Term("林媽媽")
-n["林本人"] = Term("林本人")
-n["林妹妹"] = Term("林妹妹")
-n["林太太"] = Term("林太太")
-n["林囡囡"] = Term("林囡囡")
-n["男朋友"] = Term("男朋友")
-n["張爸爸"] = Term("張爸爸")
-n["張媽媽"] = Term("張媽媽")
+# define the rules
+def create_rules(xs, ys):
+  X, Y, Z = map(Var, ["X", "Y", "Z"])
+  sp = SimpleProgram()
+  sp += xs["myself"](X, Y) << xs["myself"](X, Y)
+  sp += xs["husband"](X, Y) << xs["wife"](Y, X)
+  sp += xs["wife"](X, Y) << xs["husband"](Y, X)
+  sp += xs["couple"](X, Y) << (xs["husband"](X, Y) | xs["wife"](X, Y))
+  sp += xs["couple"](X, Y) << xs["couple"](Y, X)
+  sp += xs["son"](X, Y) << (xs["son"](X, Y) |
+                            (xs["couple"](X, Z) & xs["son"](Z, Y)))
+  sp += xs["daughter"](X, Y) << (xs["daughter"](X, Y) |
+                                 (xs["couple"](X, Z) & xs["daughter"](Z, Y)))
+  sp += xs["child"](X, Y) << (xs["son"](X, Y) | xs["daughter"](X, Y))
+  sp += xs["child"](X, Y) << (xs["father"](Y, X) | xs["mother"](Y, X))
+  sp += xs["child"](X, Y) << (xs["couple"](X, Z) & xs["child"](Z, Y))
+  sp += xs["parent"](X, Y) << xs["child"](Y, X)
+  sp += xs["mother"](X, Y) << (xs["wife"](Z, Y) & xs["child"](Y, X))
+  sp += xs["father"](X, Y) << (xs["husband"](Z, Y) & xs["child"](Y, X))
+  sp += xs["sibling"](X, Y) << (xs["parent"](X, Z) & xs["parent"](Y, Z))
+  sp += xs["grandfather"](X, Y) << (xs["father"](X, Z) & xs["father"](Z, Y))
+  sp += xs["grandmother"](X, Y) << (xs["father"](X, Z) & xs["mother"](Z, Y))
+  sp += xs["grandparents"](
+      X, Y) << (xs["grandfather"](X, Y) | xs["grandmother"](X, Y))
+  sp += xs["grandson"](X, Y) << (xs["son"](X, Z) & xs["son"](Z, Y))
+  sp += xs["granddaughter"](X, Y) << (xs["son"](X, Z) & xs["daughter"](Z, Y))
+  sp += xs["grandchildren"](X, Y) << (xs["son"](X, Z) & xs["child"](Z, Y))
+  sp += xs["grandfather_"](X, Y) << (xs["mother"](X, Z) & xs["father"](Z, Y))
+  sp += xs["grandmother_"](X, Y) << (xs["mother"](X, Z) & xs["mother"](Z, Y))
+  sp += xs["grandparents_"](
+      X, Y) << (xs["grandfather_"](X, Y) | xs["grandmother_"](X, Y))
+  sp += xs["grandson_"](X, Y) << (xs["daughter"](X, Z) & xs["son"](Z, Y))
+  sp += xs["granddaughter_"](
+      X, Y) << (xs["daughter"](X, Z) & xs["daughter"](Z, Y))
+  sp += xs["grandchildren_"](X,
+                             Y) << (xs["daughter"](X, Z) & xs["child"](Z, Y))
+  sp += xs["son-wife"](X, Y) << (xs["husband"](Y, Z) & xs["parent"](Z, X))
+  sp += xs["daughter-husband"](X, Y) << (xs["wife"](Y, Z) & xs["parent"](Z, X))
+  sp += xs["daughter-husband"](X, Y) << (xs["wife"](Y, Z) & xs["parent"](Z, X))
+  sp += xs["husband-mother"](X,
+                             Y) << (xs["husband"](X, Z) & xs["mother"](Z, Y))
+  sp += xs["husband-father"](X,
+                             Y) << (xs["husband"](X, Z) & xs["father"](Z, Y))
+  sp += xs["wife-mother"](X, Y) << (xs["wife"](X, Z) & xs["mother"](Z, Y))
+  sp += xs["wife-father"](X, Y) << (xs["wife"](X, Z) & xs["father"](Z, Y))
 
-sp += t["wife"](n["林爸爸"], n["林媽媽"])         # 林媽媽 是 林爸爸 的 妻子
-sp += t["son"](n["林爸爸"], n["林本人"])          # 林本人 是 林爸爸 的 兒子
-sp += t["daughter"](n["林媽媽"], n["林妹妹"])     # 林妹妹 是 林媽媽 的 女兒
-sp += t["husband"](n["林太太"], n["林本人"])      # 林本人 是 林太太 的 丈夫
-sp += t["father"](n["林囡囡"], n["林本人"])       # 林本人 是 林囡囡 的 爸爸
-sp += t["husband"](n["林妹妹"], n["男朋友"])      # 男朋友 是 林妹妹 的 丈夫
-sp += t["mother"](n["林太太"], n["張媽媽"])       # 張媽媽 是 林太太 的 媽媽
-sp += t["wife"](n["張爸爸"], n["張媽媽"])         # 張媽媽 是 張爸爸 的 妻子
+  sp += xs["wife"](ys["林爸爸"], ys["林媽媽"])  # 林媽媽 是 林爸爸 的 妻子
+  sp += xs["son"](ys["林爸爸"], ys["林本人"])  # 林本人 是 林爸爸 的 兒子
+  sp += xs["daughter"](ys["林媽媽"], ys["林妹妹"])  # 林妹妹 是 林媽媽 的 女兒
+  sp += xs["husband"](ys["林太太"], ys["林本人"])  # 林本人 是 林太太 的 丈夫
+  sp += xs["father"](ys["林囡囡"], ys["林本人"])  # 林本人 是 林囡囡 的 爸爸
+  sp += xs["husband"](ys["林妹妹"], ys["男朋友"])  # 男朋友 是 林妹妹 的 丈夫
+  sp += xs["mother"](ys["林太太"], ys["張媽媽"])  # 張媽媽 是 林太太 的 媽媽
+  sp += xs["wife"](ys["張爸爸"], ys["張媽媽"])  # 張媽媽 是 張爸爸 的 妻子
+  return sp
 
-for i in n:
-    for j in n:
-        if i == j:
-            continue
-        print_relationship(n[j], n[i], t, sp)
-    print("")
+# inference and show all possible result
+def do_inference(a, b, terms, program):
+  engine = DefaultEngine()
+  xs = engine.prepare(program)
+
+  print("[%s]是[%s]的 --> " % (b, a), end="")
+  for key in terms:
+    query_term = terms[key](a, b)
+    res = engine.query(xs, query_term)
+    if bool(res):
+      print(terms[key], end=" ")
+  print("")
+
+# test suite
+def test_inference():
+  xs = file_manager("family_class.db").get_dict()
+  ys = file_manager("family_instance.db").get_dict()
+  sp = create_rules(xs, ys)
+  ret = [do_inference(ys[j], ys[i], xs, sp) for i in ys for j in ys if i != j]
+  assert None != ret
+
+# entry point
+if __name__ == '__main__':
+  test_inference()
+  print("")
