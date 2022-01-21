b7a61f99674ac528dc4528b1526258e20770fde0
Kinda Lam <3446627+supercatex@users.noreply.github.com>
2022-01-21 16:44:52 +0800

    Update yolov5_ros_demo.py

--- ros/yolov5_ros_demo.py
+++ ros/yolov5_ros_demo.py
@@ -7,14 +7,14 @@ import os
 import torch
 
 def callback_image(msg):
-    global frame
-    frame = np.frombuffer(
+    global _frame
+    _frame = np.frombuffer(
                 msg.data, dtype=np.uint8
             ).reshape(msg.height, msg.width, -1)[:, :, ::-1]
 
 def callback_depth(msg):
-    global depth
-    depth = np.frombuffer(
+    global _depth
+    _depth = np.frombuffer(
                 msg.data, dtype=np.uint16
             ).reshape(msg.height, msg.width)
 
@@ -28,27 +28,29 @@ if __name__ == "__main__":
     os.environ["TORCH_HOME"] = "~"
     net = torch.hub.load("ultralytics/yolov5", "yolov5l", device="cpu")
 
-    frame = None
-    depth = None
+    _frame = None
+    _depth = None
     while not rospy.is_shutdown():
         rospy.Rate(20).sleep()
-        if frame is None: continue
-        if depth is None: continue
+        if _frame is None: continue
+        if _depth is None: continue
 
-        img = frame.copy()
-        res = net(img)
+        frame = _frame.copy()
+        depth = _depth.copy()
+
+        res = net(frame)
         for x1, y1, x2, y2, pred, index in res.xyxy[0]:
             if pred < 0.7: continue
             x1, y1, x2, y2, index = map(int, (x1, y1, x2, y2, index))
             name = net.names[index]
-            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
             cv2.putText(
-                img, name, (x1, y1 + 20),
+                frame, name, (x1, y1 + 20),
                 cv2.FONT_HERSHEY_SIMPLEX, 0.8, 
                 (0, 255, 0), 1, cv2.LINE_AA
             )
 
-        cv2.imshow("frame", img)
+        cv2.imshow("frame", frame)
         cv2.imshow("depth", depth)
         key_code = cv2.waitKey(1)
         if key_code in [27, ord('q')]:
