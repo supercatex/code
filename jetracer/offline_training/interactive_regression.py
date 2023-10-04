59f001b61d25aa2726061e2b3e61334976f0cf9f
Kinda Lam <3446627+supercatex@users.noreply.github.com>
2023-10-04 13:50:16 +0800

    Update interactive_regression.py

--- jetracer/offline_training/interactive_regression.py
+++ jetracer/offline_training/interactive_regression.py
@@ -3,12 +3,13 @@ from xy_dataset import XYDataset
 import torch
 import torchvision
 import torch.utils.data
+import os 
 
 
 TASK = 'road_following'
 CATEGORIES = ['apex']
 DATASETS = ['A', 'B']
-MODEL_NAME = "my_model.pt"
+MODEL_PATH = "my_model.pt"
 
 EPOCHS = 50
 BATCH_SIZE = 8
@@ -34,65 +35,48 @@ model = torchvision.models.resnet18(pretrained=True)
 model.fc = torch.nn.Linear(512, output_dim)
 
 model = model.to(device)
-# model.load_state_dict(torch.load(MODEL_NAME))  # using existed model.
+if os.path.exists(MODEL_PATH):
+    model.load_state_dict(torch.load(MODEL_PATH))  # using existed model.
 
 optimizer = torch.optim.Adam(model.parameters())
 
+try:
+    train_loader = torch.utils.data.DataLoader(
+        dataset,
+        batch_size=BATCH_SIZE,
+        shuffle=True
+    )
+    model = model.train()
 
-def train_eval(is_training):
-    global EPOCHS, BATCH_SIZE, LEARNING_RATE, model, dataset, optimizer
+    epoch = 0
+    while epoch < EPOCHS:
+        i = 0
+        sum_loss = 0.0
+        for images, category_idx, xy in iter(train_loader):
+            # send data to device
+            images = images.to(device)
+            xy = xy.to(device)
+            optimizer.zero_grad()
 
-    try:
-        train_loader = torch.utils.data.DataLoader(
-            dataset,
-            batch_size=BATCH_SIZE,
-            shuffle=True
-        )
+            # execute model to get outputs
+            outputs = model(images)
 
-        if is_training:
-            model = model.train()
-        else:
-            model = model.eval()
+            # compute MSE loss over x, y coordinates for associated categories
+            loss = torch.tensor(0.0)
+            for batch_idx, cat_idx in enumerate(list(category_idx.flatten())):
+                loss += torch.mean((outputs[batch_idx][2 * cat_idx:2 * cat_idx + 2] - xy[batch_idx]) ** 2)
+            loss /= len(category_idx)
 
-        epoch = 0
-        while epoch < EPOCHS:
-            i = 0
-            sum_loss = 0.0
-            for images, category_idx, xy in iter(train_loader):
-                # send data to device
-                images = images.to(device)
-                xy = xy.to(device)
+            loss.backward()
+            optimizer.step()
 
-                if is_training:
-                    # zero gradients of parameters
-                    optimizer.zero_grad()
+            # increment progress
+            count = len(category_idx.flatten())
+            i += count
+            sum_loss += float(loss)
 
-                # execute model to get outputs
-                outputs = model(images)
-
-                # compute MSE loss over x, y coordinates for associated categories
-                loss = torch.tensor(0.0)
-                for batch_idx, cat_idx in enumerate(list(category_idx.flatten())):
-                    loss += torch.mean((outputs[batch_idx][2 * cat_idx:2 * cat_idx + 2] - xy[batch_idx]) ** 2)
-                loss /= len(category_idx)
-
-                if is_training:
-                    # run backpropagation to accumulate gradients
-                    loss.backward()
-
-                    # step optimizer to adjust parameters
-                    optimizer.step()
-
-                # increment progress
-                count = len(category_idx.flatten())
-                i += count
-                sum_loss += float(loss)
-
-            if is_training:
-                epoch += 1
-                torch.save(model.state_dict(), MODEL_NAME)
-            else:
-                break
-    except Exception as e:
-        print(e)
-    model = model.eval()
+        epoch += 1
+        torch.save(model.state_dict(), MODEL_PATH)
+except Exception as e:
+    print(e)
+model = model.eval()
