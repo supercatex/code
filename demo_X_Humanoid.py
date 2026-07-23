dbb3d59d150fc19cbd5189d62a41dba8ee0756ce
Kinda Lam <3446627+supercatex@users.noreply.github.com>
2026-07-23 11:49:11 +0800

    Update demo_X_Humanoid.py

--- demo_X_Humanoid.py
+++ demo_X_Humanoid.py
@@ -16,6 +16,7 @@ from geometry_msgs.msg import Pose
 import time 
 from bodyctrl_msgs.srv import SetAngleFlexible, SetForce, SetGestureForceCalibration
 from lyre_msgs.srv import PlayText 
+import requests
 
 
 class MotorStatus(object):
@@ -174,25 +175,22 @@ class XarmHandler(object):
         param_configer = ParamConfiger(self.xarm)
         param_configer.set_node_parameter("endpose_single_arm_qp_L_controller", "dis_err_bound", 10.0)
         param_configer.set_node_parameter("endpose_single_arm_qp_L_controller", "ori_err_bound", 3.0)
-        param_configer.set_node_parameter("endpose_single_arm_qp_L_controller", "otg_p_step", 0.002)
-        param_configer.set_node_parameter("endpose_single_arm_qp_L_controller", "otg_r_step", 0.002)
+        param_configer.set_node_parameter("endpose_single_arm_qp_L_controller", "otg_p_step", 0.0005)
+        param_configer.set_node_parameter("endpose_single_arm_qp_L_controller", "otg_r_step", 0.0005)
 
         param_configer.set_node_parameter("endpose_single_arm_qp_R_controller", "dis_err_bound", 10.0)
         param_configer.set_node_parameter("endpose_single_arm_qp_R_controller", "ori_err_bound", 3.0)
-        param_configer.set_node_parameter("endpose_single_arm_qp_R_controller", "otg_p_step", 0.002)
-        param_configer.set_node_parameter("endpose_single_arm_qp_R_controller", "otg_r_step", 0.002)
+        param_configer.set_node_parameter("endpose_single_arm_qp_R_controller", "otg_p_step", 0.0005)
+        param_configer.set_node_parameter("endpose_single_arm_qp_R_controller", "otg_r_step", 0.0005)
 
         param_configer.set_node_parameter("endpose_dual_arm_qp_controller", "dis_err_bound", 10.0)
         param_configer.set_node_parameter("endpose_dual_arm_qp_controller", "ori_err_bound", 3.0)
-        param_configer.set_node_parameter("endpose_dual_arm_qp_controller", "otg_p_step", 0.002)
-        param_configer.set_node_parameter("endpose_dual_arm_qp_controller", "otg_r_step", 0.002)
-        self.activate()
+        param_configer.set_node_parameter("endpose_dual_arm_qp_controller", "otg_p_step", 0.0005)
+        param_configer.set_node_parameter("endpose_dual_arm_qp_controller", "otg_r_step", 0.0005)
 
-    def activate(self):
         self.xarm.hardware_arm_enable(True)
 
     def deactivate(self):
-        self.xarm.hardware_all_enable(False)
         self.xarm.xarm_deactivate_all_controller()
         self.xarm.get_logger().info("deactivate all controller.")
 
@@ -212,6 +210,7 @@ class XarmHandler(object):
         msg_pose.orientation.z = q[2]
         msg_pose.orientation.w = q[3]
 
+        self.xarm.xarm_deactivate_all_controller()
         self.xarm.activate_controller("endpose_single_arm_qp_L_controller")
         self.topic_publisher.publish_endposetarget_L(msg_pose, from_frame="waist_yaw_link")
         time.sleep(delay)
@@ -233,6 +232,7 @@ class XarmHandler(object):
         msg_pose.orientation.z = q[2]
         msg_pose.orientation.w = q[3]
 
+        self.xarm.xarm_deactivate_all_controller()
         self.xarm.activate_controller("endpose_single_arm_qp_R_controller")
         self.topic_publisher.publish_endposetarget_R(msg_pose, from_frame="waist_yaw_link")
         time.sleep(delay)
@@ -272,6 +272,7 @@ class XarmHandler(object):
         self.xarm.activate_controller("endpose_dual_arm_qp_controller")
         self.topic_publisher.publish_endposetarget_dual(msg_pose1, msg_pose2, from_frame_l="waist_yaw_link", from_frame_r="waist_yaw_link")
         time.sleep(delay)
+        self.xarm.xarm_deactivate_all_controller()
 
     def set_arm_joints(self, pos_L, pos_R, delay=5.0):
         self.xarm.xarm_deactivate_all_controller()
@@ -280,6 +281,165 @@ class XarmHandler(object):
         self.topic_publisher.publish_jointspace_commands_L(pos_L)
         self.topic_publisher.publish_jointspace_commands_R(pos_R)
         time.sleep(delay)
+        self.xarm.xarm_deactivate_all_controller()
+
+
+class SlamtecNode(object):
+    def __init__(self):
+        self.ip = "192.168.11.1"
+        self.port = 1448
+        self.headers = {
+            "accept": "application/json",
+            "Content-Type": "application/json"
+        }
+
+    def send_request(self, api, method="GET", json_data={}):
+        try:
+            url = f"http://{self.ip}:{self.port}{api}"
+            response = requests.request(
+                method=method,
+                url=url, 
+                headers=self.headers,
+                json=json_data
+            )
+            response.raise_for_status()
+            return response.json()
+        except Exception as e:
+            print(e)
+            return None
+        
+    def get_power_status(self):
+        return self.send_request("/api/core/system/v1/power/status")
+    
+    def get_network_status(self):
+        return self.send_request("/api/core/system/v1/network/status")
+
+    def get_network_route(self):
+        return self.send_request("/api/core/system/v1/network/route")
+    
+    def get_network_apn(self):
+        return self.send_request("/api/core/system/v1/network/apn")
+    
+    def get_localization_pose(self):
+        return self.send_request("/api/core/slam/v1/localization/pose")
+    
+    def get_localization_quality(self):
+        return self.send_request("/api/core/slam/v1/localization/quality")
+    
+    def get_localization_odopose(self):
+        return self.send_request("/api/core/slam/v1/localization/odopose")
+        
+    def get_localization_enable(self):
+        return self.send_request("/api/core/slam/v1/localization/:enable")
+    
+    def get_mapping_enable(self):
+        return self.send_request("/api/core/slam/v1/mapping/:enable")
+    
+    def get_loopclosure_enable(self):
+        return self.send_request("/api/core/slam/v1/loopclosure/:enable")
+    
+    def get_point_of_interest(self):
+        return self.send_request("/api/core/artifact/v1/pois")
+    
+    def get_action_factories(self):
+        return self.send_request("/api/core/motion/v1/action-factories")
+    
+    def get_current_action(self):
+        return self.send_request("/api/core/motion/v1/actions/:current")
+    
+    def move_to(self, x, y, yaw=0.0):
+        return self.send_request("/api/core/motion/v1/actions", "POST", {
+            "action_name": "slamtec.agent.actions.MoveToAction",
+            "options": {
+                "target": {
+                    "x": x,
+                    "y": y,
+                    "z": 0
+                },
+                "move_options": {
+                    "mode": 2,
+                    "flags": ["with_yaw"],
+                    "yaw": yaw,
+                    "acceptable_precision": 0,
+                    "fail_retry_count": 0,
+                    "speed_ratio": 0
+                }
+            }
+        })
+    
+    def get_action_status(self, action_id):
+        return self.send_request(f"/api/core/motion/v1/actions/{action_id}")
+    
+    def stop_current_action(self):
+        return self.send_request("/api/core/motion/v1/actions/:current", "DELETE")
+    
+    def move_to_home(self):
+        return self.send_request("/api/core/motion/v1/actions", "POST", {
+            "action_name": "slamtec.agent.actions.GoHomeAction",
+            "gohome_options": {
+                "flags": "dock",
+                "back_to_landing": True,
+                "charging_retry_coount": 3
+            }
+        })
+    
+    def get_poi_by_name(self, name):
+        res = self.get_point_of_interest()
+        for obj in res:
+            if obj["metadata"]["display_name"] == name:
+                return obj["pose"]["x"], obj["pose"]["y"], obj["pose"]["yaw"]
+        return None 
+
+    def set_localization_pose(self, x, y, yaw):
+        return self.send_request("/api/core/slam/v1/localization/pose", "PUT", {
+            "x": x,
+            "y": y,
+            "z": 0,
+            "yaw": yaw,
+            "pitch": 0,
+            "roll": 0
+        })
+
+    def set_localization_enable(self, enable):
+        return self.send_request("/api/core/slam/v1/localization/:enable", "PUT", {
+            "enable": enable
+        })
+
+    def set_mapping_enable(self, enable):
+        return self.send_request("/api/core/slam/v1/mapping/:enable", "PUT", {
+            "enable": enable
+        })
+
+    def set_loopclosure_enable(self, enable):
+        return self.send_request("/api/core/slam/v1/loopclosure/:enable", "PUT", {
+            "enable": enable
+        })
+
+    def reset_localization_status(self):
+        return self.send_request("/api/core/slam/v1/localization/status/:reset", "POST")
+
+    def set_stcm_map(self, filepath):
+        with open(filepath, "rb") as f:
+            try:
+                url = f"http://{self.ip}:{self.port}/api/core/slam/v1/maps/stcm"
+                response = requests.request(
+                    method="PUT",
+                    url=url, 
+                    headers={
+                        "accept": "*/*",
+                        "Content-Type": "application/octet-stream"
+                    },
+                    data=f
+                )
+                response.raise_for_status()
+                return True
+            except Exception as e:
+                print(e)
+                return False
+        return False
+
+    def clear_stcm_map(self):
+        return self.send_request("/api/core/slam/v1/maps", "DELETE")
 
 
 class X_Humanoid(object):
@@ -357,10 +517,10 @@ class X_Humanoid(object):
         self.waist_status.update(msg)
 
     def callback_leg_status(self, msg):
-        print("LEG update")
         self.leg_status.update(msg)
 
     def check_head(self):
+        time.sleep(3)
         if self.head_image is None: self.node.get_logger().warn("head image is not found."); return False
         if self.head_depth is None: self.node.get_logger().warn("head depth is not found."); return False 
         return True
@@ -398,25 +558,33 @@ class X_Humanoid(object):
         msg.cmds.append(self.create_SetMotorSpeed(3, z, 0.5))
         self.head_cmd_vel.publish(msg)
 
-    def set_waist_pos(self, p1, p2):
+    def set_waist_pos(self, p1, p2, deg=False, delay=5.0):
+        if deg: 
+            p1 = np.deg2rad(p1)
+            p2 = np.deg2rad(p2)
         msg = CmdSetMotorPosition()
         msg.cmds.append(self.create_SetMotorPosition(31, p1, 0.3, 0.5))
         msg.cmds.append(self.create_SetMotorPosition(32, p2, 0.3, 0.5))
         self.waist_cmd_pos.publish(msg)
+        time.sleep(delay)
 
-    def set_leg_pos(self, p1, p2):
+    def set_leg_pos(self, p1, p2, deg=False, delay=5.0):
+        if deg: 
+            p1 = np.deg2rad(p1)
+            p2 = np.deg2rad(p2)
         msg = CmdSetMotorPosition()
         msg.cmds.append(self.create_SetMotorPosition(51, p1, 0.3, 0.5))
         msg.cmds.append(self.create_SetMotorPosition(52, p2, 0.3, 0.5))
         self.leg_cmd_pos.publish(msg)
+        time.sleep(delay)
 
     def set_height(self, h, delay=5.0):
         L = 240
         h = max(150, min(h, 450))
         a = np.deg2rad(60) - np.arccos(h / 2 / L)
 
-        self.set_waist_pos(self.waist_status.motors[0].pos, a)
-        self.set_leg_pos(-a, a)
+        self.set_waist_pos(self.waist_status.motors[0].pos, a, delay=0.0)
+        self.set_leg_pos(-a, a, delay=0.0)
         time.sleep(delay)
     
     def get_height(self):
@@ -429,6 +597,10 @@ class X_Humanoid(object):
         pos_L = [0.64, -0.15, 0.0, -1.31, 0.0, -0.48, -0.35]
         pos_R = [0.64, 0.15, 0.0, -1.31, 0.0, -0.48, 0.35]
         self.arm.set_arm_joints(pos_L, pos_R, delay)
+
+    def set_home_pose(self, delay=5.0):
+        self.set_height(240, delay=0.0)
+        self.set_arm_home_pose(delay=delay)
     
     def set_left_hand(self, angle1, angle2, angle3, force=0.1, delay=3.0):
         request = SetForce.Request()
@@ -474,11 +646,11 @@ class X_Humanoid(object):
         self.set_gesture_force_calibration_R.call_async(request)
         time.sleep(delay)
 
-    def say(self, text):
+    def say(self, text, delay=1.0):
         request = PlayText.Request()
-        request.text = "Hello World"
+        request.text = text
         self.play_text.call_async(request)
-        time.sleep(3)
+        time.sleep(delay)
 
 
 class Main(Node):
@@ -488,48 +660,112 @@ class Main(Node):
         self.timer = self.create_timer(1 / 20, self.callback_timer)
 
         self.robot.arm.deactivate()
+        self.robot.set_home_pose()
+
+        self.base = SlamtecNode()
+
+        self.robot.say("大家好")
+        # self.robot.say("當前電量還有百分之%d" % self.base.get_power_status()["batteryPercentage"])
+        self.robot.say("正在截入地圖並設置初始點")
+        print(self.base.set_stcm_map("/home/ubuntu/workspace/map_001.stcm"))
+        print(self.base.set_localization_pose(-2.74, 0.39, -0.05))
+        time.sleep(3)
         
-        self.robot.say("我準備好了")
-        # print("END")
+        self.robot.say("我準備好了，開始檢測鏡頭")
         time.sleep(3)
 
     def callback_timer(self):
         if not self.robot.check_head(): return 
 
-        # print(self.robot.head_status)
-        # print(self.robot.waist_status)
-        # print(self.robot.leg_status)
-
-
+        self.robot.say("正在檢測頭部運動")
         self.robot.set_head_pos((0, 25, 0), deg=True)
         self.robot.set_head_pos((0, 0, 80), deg=True)
         self.robot.set_head_pos((0, -25, 0), deg=True)
         self.robot.set_head_pos((0, 0, -80), deg=True)
         self.robot.set_head_pos((0, 0, 0), deg=True)
-        
+
+        self.robot.say("正在檢測高度調節")
         self.robot.set_height(150)
         self.robot.set_height(450)
         self.robot.set_height(240)
 
-        # self.robot.set_height(300)
-        # self.robot.set_waist_pos(0.0, 0.8)
-        # time.sleep(100)
+        self.robot.say("彎腰測試")
+        self.robot.set_waist_pos(0.0, 30, deg=True)
+        self.robot.set_waist_pos(0.0, -30, deg=True)
+        self.robot.set_waist_pos(0.0, 0.0)
 
-        self.robot.arm.set_endpose_LR((0.2, 0.4, 0.0), (0.0, -90, 0.0), (0.2, -0.4, 0.0), (0.0, -90, 0.0), deg=True, delay=3.0)
-        # self.robot.arm.xarm.hardware_arm_enable(True)
+        self.robot.say("展開雙臂")
+        self.robot.arm.set_endpose_LR((0.2, 0.4, 0.0), (0.0, -90, 0.0), (0.2, -0.4, 0.0), (0.0, -90, 0.0), deg=True, delay=5.0)
 
-        self.robot.set_arm_home_pose()
+        self.robot.say("舉高雙臂")
+        self.robot.arm.set_endpose_LR((0.2, 0.4, 0.5), (0.0, -90, 0.0), (0.2, -0.4, 0.5), (0.0, -90, 0.0), deg=True, delay=5.0)
 
-        self.robot.check_left_hand()
-        self.robot.check_right_hand()
+        self.robot.say("左臂測試")
+        self.robot.arm.set_endpose_L((0.2, 0.4, 0.0), (0.0, -90, 0.0), deg=True, delay=5.0)
 
+        self.robot.say("右臂測試")
+        self.robot.arm.set_endpose_R((0.2, -0.4, 0.0), (0.0, -90, 0.0), deg=True, delay=5.0)
+
+        self.robot.say("移動姿態")
+        self.robot.set_home_pose()
+
+        self.robot.say("檢測靈巧手")
+        self.robot.check_left_hand(delay=0.0)
+        self.robot.check_right_hand(delay=15.0)
+
+        self.robot.say("左手抓取測試")
         self.robot.set_left_hand(1.0, 1.0, 1.0)
         self.robot.set_left_hand(1.0, 1.0, 0.0)
         self.robot.set_left_hand(0.6, 0.3, 0.0)
 
+        self.robot.say("右手抓取測試")
         self.robot.set_right_hand(1.0, 1.0, 1.0)
         self.robot.set_right_hand(1.0, 1.0, 0.0)
         self.robot.set_right_hand(0.6, 0.3, 0.0)
+
+        state = 0
+        action = None
+        while True:
+            time.sleep(0.5)
+            if state == 0:
+                self.robot.say("正在前往P1")
+                p1 = self.base.get_poi_by_name("P1")
+                action = self.base.move_to(*p1)
+                state = 1
+            elif state == 1:
+                action_state = self.base.get_action_status(action["action_id"])
+                print(action_state)
+                if action_state["state"]["status"] == 4:
+                    state = 2
+            elif state == 2:
+                self.robot.say("正在前往P2")
+                p2 = self.base.get_poi_by_name("P2")
+                action = self.base.move_to(*p2)
+                state = 3
+            elif state == 3:
+                action_state = self.base.get_action_status(action["action_id"])
+                print(action_state)
+                if action_state["state"]["status"] == 4:
+                    state = 4
+            elif state == 4:
+                self.robot.say("正在前往P3")
+                p3 = self.base.get_poi_by_name("P3")
+                action = self.base.move_to(*p3)
+                state = 5
+            elif state == 5:
+                action_state = self.base.get_action_status(action["action_id"])
+                print(action_state)
+                if action_state["state"]["status"] == 4:
+                    state = 6
+            elif state == 6:
+                self.robot.say("回家了")
+                self.base.move_to_home()
+                state = 7
+            elif state == 7:
+                action_state = self.base.get_action_status(action["action_id"])
+                print(action_state)
+                if action_state["state"]["status"] == 4:
+                    break
 
         cv2.imshow("frame", self.robot.head_image)
         cv2.imshow("depth", self.robot.head_depth)
