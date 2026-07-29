2d099031bfb8e57df576ae6cb4bae5776478f2e1
Kinda Lam <3446627+supercatex@users.noreply.github.com>
2026-07-29 11:39:02 +0800

    Update basic.py

--- ros2/KindaGarten/basic.py
+++ ros2/KindaGarten/basic.py
@@ -8,7 +8,8 @@ from rclpy.executors import SingleThreadedExecutor
 
 import cv2
 import numpy as np 
-import time, base64, json, copy 
+import time, base64, json, copy, requests
+from ollama import Client
 
 
 class SingleThreadedExecutorInstance(object):
@@ -39,3 +40,34 @@ class SubscriptionNode(Node):
             self.data = self.func(msg)
         else:
             self.data = msg 
+
+
+def get_real_xyz(x, y, depth, rpy=(0, 0, 0), k = 0):
+    d = depth[y][x]
+    h, w = depth.shape
+    for dx in range(-k, k + 1, 1):
+        for dy in range(-k, k + 1, 1):
+            if y + dy < 0 or y + dy >= h: continue 
+            if x + dx < 0 or x + dx >= w: continue
+            if depth[y + dy][x + dx] == 0: continue 
+            if d == 0 or 0 < depth[y + dy][x + dx] < d:
+                d = depth[y + dy][x + dx]
+    x, y = x - w // 2, y - h // 2
+    a, b = np.deg2rad(86), np.deg2rad(55)
+    # x / w = real_x / real_w
+    # real_w / 2 / d = tan(a / 2)
+    real_x = 2 * d * np.tan(a / 2) * x / w 
+    real_y = 2 * d * np.tan(b / 2) * y / h 
+    # real_x = x * d / 620
+    # real_y = y * d / 620
+    real_x, real_y, real_z = d, -real_x, -real_y + 600
+
+    rx, ry, rz = rpy
+    ry = np.deg2rad(np.rad2deg(ry) + 21)
+    # print(rpy)
+
+    return real_x / 1000, real_y / 1000, real_z / 1000
+
+
+if __name__ == "__main__":
+    print(get_real_xyz(100, 200, np.zeros((720, 1280))))
