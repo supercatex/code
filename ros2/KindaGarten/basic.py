a4867aaa51614705f269b0bb001f640e453c7d55
Kinda Lam <3446627+supercatex@users.noreply.github.com>
2026-08-03 12:41:51 +0800

    Update basic.py

--- ros2/KindaGarten/basic.py
+++ ros2/KindaGarten/basic.py
@@ -42,7 +42,7 @@ class SubscriptionNode(Node):
             self.data = msg 
 
 
-def get_real_xyz(x, y, depth, rpy=(0, 0, 0), k = 0):
+def get_real_xyz(x, y, depth, rpy=(0, 0, 0), k=0):
     d = depth[y][x]
     h, w = depth.shape
     for dx in range(-k, k + 1, 1):
@@ -65,6 +65,58 @@ def get_real_xyz(x, y, depth, rpy=(0, 0, 0), k = 0):
     # real_x, real_z = new_x, new_z
 
     return real_x / 1000, real_y / 1000, (real_z + 600) / 1000
+
+
+def get_real_xyz_v2(x, y, depth, rpy=(0, 0, 0), k=0):
+    d = depth[y][x]
+    h, w, = depth.shape 
+    for dx in range(-k, k + 1, 1):
+        for dy in range(-k, k + 1, 1):
+            if y + dy < 0 or y + dy >= h: continue 
+            if x + dx < 0 or x + dx >= w: continue
+            if depth[y + dy][x + dx] == 0: continue 
+            if d == 0 or 0 < depth[y + dy][x + dx] < d:
+                d = depth[y + dy][x + dx]
+    x, y = x - w // 2, y - h // 2
+    
+    a, b = np.deg2rad(86), np.deg2rad(55)
+    real_x = 2 * d * np.tan(a / 2) * x / w 
+    real_y = 2 * d * np.tan(b / 2) * y / h 
+    pos = np.array([d, -real_x, -real_y], dtype=np.float32)
+
+    rpy = np.array(rpy, dtype=np.float32) + (0.0, np.deg2rad(30), 0.0)
+    roll, pitch, yaw = rpy
+    pos = pos / 1000 + (0.08, 0.00, 0.08)
+    pos = rot_z(yaw) @ rot_y(pitch) @ rot_x(roll) @ pos
+    pos = pos + (0.0, 0.0, 0.64)
+    return pos
+
+def rot_x(roll):
+    c = np.cos(roll)
+    s = np.sin(roll)
+    return np.array([
+        [1.0, 0.0, 0.0],
+        [0.0, c  , -s ],
+        [0.0, s  ,  c ]
+    ], dtype=np.float64)
+
+def rot_y(pitch):
+    c = np.cos(pitch)
+    s = np.sin(pitch)
+    return np.array([
+        [c  , 0.0, s  ],
+        [0.0, 1.0, 0.0],
+        [-s , 0.0, c  ]
+    ], dtype=np.float64)
+
+def rot_z(yaw):
+    c = np.cos(yaw)
+    s = np.sin(yaw)
+    return np.array([
+        [c  , -s , 0.0],
+        [s  , c  , 0.0],
+        [0.0, 0.0, 1.0]
+    ], dtype=np.float64)
 
 
 if __name__ == "__main__":
