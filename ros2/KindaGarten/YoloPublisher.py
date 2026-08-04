5829988d32999b9b24c603b6a4d5fdfee31942c4
Kinda Lam <3446627+supercatex@users.noreply.github.com>
2026-08-05 00:17:27 +0800

    Update YoloPublisher.py

--- ros2/KindaGarten/YoloPublisher.py
+++ ros2/KindaGarten/YoloPublisher.py
@@ -1,22 +1,8 @@
-import rclpy
-from rclpy.node import Node 
-from sensor_msgs.msg import Image 
-from rclpy.qos import qos_profile_sensor_data
-from std_msgs.msg import String
-import cv2 
-import numpy as np 
-import time 
-from ultralytics import YOLO
-import base64
-import json 
-import copy
-
-
 class YoloPublisher(Node):
     def __init__(self):
         super().__init__("YoloPublisher")
-        
-        self.msg_image = None 
+
+        self.msg_image = None
         self.msg_depth = None
         self.create_subscription(
             Image,
@@ -29,7 +15,7 @@ class YoloPublisher(Node):
             self.callback_depth, qos_profile_sensor_data
         )
         self.pub_yolo = self.create_publisher(
-            String, 
+            String,
             "/ob_camera_head/yolo/obj", 10
         )
         self.pub_image = self.create_publisher(
@@ -41,7 +27,13 @@ class YoloPublisher(Node):
             "/ob_camera_head/yolo/depth", 10
         )
 
-        self.yolo = YOLO("yolo26x-seg.pt")
+        self.models = {}
+        self.yolo_dir = "/data/yolo/"
+        for name in os.listdir(self.yolo_dir):
+            if name[-3:] != ".pt": continue
+            path = os.path.join(self.yolo_dir, name)
+            self.models[name] = YOLO(path)
+
 
     def callback_image(self, msg): self.msg_image = msg
     def callback_depth(self, msg): self.msg_depth = msg
@@ -57,7 +49,7 @@ if __name__ == "__main__":
         if node.msg_image is None or node.msg_depth is None:
             node.get_logger().warn("Image or depth not received.")
             time.sleep(3)
-            continue 
+            continue
 
         msg_image = copy.deepcopy(node.msg_image)
         msg_depth = copy.deepcopy(node.msg_depth)
@@ -65,34 +57,38 @@ if __name__ == "__main__":
         depth = np.frombuffer(msg_depth.data, dtype=np.uint16).reshape((msg_depth.height, msg_depth.width))
         image = np.array(image, dtype=np.uint8)
         depth = np.array(depth, dtype=np.int32)
+        rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-        res = node.yolo.predict(image, device="0", conf=0.1, verbose=False)[0]
-        
         data = {}
-        data["obj"] = []
-        if res.masks is not None:
-            for c, xyxy, conf, xy in zip(res.boxes.cls, res.boxes.xyxy, res.boxes.conf, res.masks.xy):
-                x1, y1, x2, y2 = map(int, xyxy)
-                cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
-                cz = depth[cy][cx]
-                data["obj"].append({
-                    "cls": int(c),
-                    "xyxy": [x1, y1, x2, y2],
-                    "conf": float(conf),
-                    "depth": int(cz),
-                    "xy": xy.tolist()
-                })
-        else:
-            for c, xyxy, conf in zip(res.boxes.cls, res.boxes.xyxy, res.boxes.conf):
-                x1, y1, x2, y2 = map(int, xyxy)
-                cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
-                cz = depth[cy][cx]
-                data["obj"].append({
-                    "cls": int(c),
-                    "xyxy": [x1, y1, x2, y2],
-                    "conf": float(conf),
-                    "depth": int(cz)
-                })
+        for name, model in node.models.items():
+            data[name] = []
+            res = model.predict(image, device="0", conf=0.1, verbose=False)[0]
+
+            if res.masks is not None:
+                for c, xyxy, conf, xy in zip(res.boxes.cls, res.boxes.xyxy, res.boxes.conf, res.masks.xy):
+                    x1, y1, x2, y2 = map(int, xyxy)
+                    cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+                    cz = depth[cy][cx]
+                    data[name].append({
+                        "cls": int(c),
+                        "xyxy": [x1, y1, x2, y2],
+                        "conf": float(conf),
+                        "depth": int(cz),
+                        "xy": xy.tolist()
+                    })
+            else:
+                for c, xyxy, conf in zip(res.boxes.cls, res.boxes.xyxy, res.boxes.conf):
+                    x1, y1, x2, y2 = map(int, xyxy)
+                    cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+                    cz = depth[cy][cx]
+                    data[name].append({
+                        "cls": int(c),
+                        "xyxy": [x1, y1, x2, y2],
+                        "conf": float(conf),
+                        "depth": int(cz)
+                    })
+
+            print(name, len(res))
 
         json_string = json.dumps(data)
         msg = String()
