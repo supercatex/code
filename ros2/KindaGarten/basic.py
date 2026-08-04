5e564eb0129e046611982d5a2a9c1e4113689c50
Kinda Lam <3446627+supercatex@users.noreply.github.com>
2026-08-05 00:14:35 +0800

    Update basic.py

--- ros2/KindaGarten/basic.py
+++ ros2/KindaGarten/basic.py
@@ -42,54 +42,45 @@ class SubscriptionNode(Node):
             self.data = msg 
 
 
-def get_real_xyz(x, y, depth, rpy=(0, 0, 0), k=0):
-    d = depth[y][x]
+def find_depth(x, y, depth, w=0, h=0):
+    x1, x2 = x - w // 2, x + w // 2 + 1
+    y1, y2 = y - h // 2, y + h // 2 + 1
+    m = np.array(depth[y1:y2, x1:x2], np.int32)
+    m = np.where(m == 0, np.inf, m)
+    d = np.min(m)
+    return d 
+
+
+def get_real_xyz_base_on_camera(x, y, depth, w=0, h=0):
+    d = find_depth(x, y, depth, w, h)
     h, w = depth.shape
-    for dx in range(-k, k + 1, 1):
-        for dy in range(-k, k + 1, 1):
-            if y + dy < 0 or y + dy >= h: continue 
-            if x + dx < 0 or x + dx >= w: continue
-            if depth[y + dy][x + dx] == 0: continue 
-            if d == 0 or 0 < depth[y + dy][x + dx] < d:
-                d = depth[y + dy][x + dx]
     x, y = x - w // 2, y - h // 2
-    a, b = np.deg2rad(86), np.deg2rad(55)
-    real_x = 2 * d * np.tan(a / 2) * x / w 
-    real_y = 2 * d * np.tan(b / 2) * y / h 
-    real_x, real_y, real_z = d, -real_x, -real_y
 
-    # rx, ry, rz = rpy
-    # ry = np.deg2rad(np.rad2deg(ry) + 21)
-    # new_z = (real_x - real_z * np.tan(ry)) * np.sin(ry) + real_z / np.cos(ry)
-    # new_x = real_z * np.tan(ry) * np.cos(ry)
-    # real_x, real_z = new_x, new_z
-
-    return real_x / 1000, real_y / 1000, (real_z + 600) / 1000
-
-
-def get_real_xyz_v2(x, y, depth, rpy=(0, 0, 0), k=0):
-    d = depth[y][x]
-    h, w, = depth.shape 
-    for dx in range(-k, k + 1, 1):
-        for dy in range(-k, k + 1, 1):
-            if y + dy < 0 or y + dy >= h: continue 
-            if x + dx < 0 or x + dx >= w: continue
-            if depth[y + dy][x + dx] == 0: continue 
-            if d == 0 or 0 < depth[y + dy][x + dx] < d:
-                d = depth[y + dy][x + dx]
-    x, y = x - w // 2, y - h // 2
-    
-    a, b = np.deg2rad(86), np.deg2rad(55)
+    a, b = np.deg2rad(90), np.deg2rad(55)
     real_x = 2 * d * np.tan(a / 2) * x / w 
     real_y = 2 * d * np.tan(b / 2) * y / h 
     pos = np.array([d, -real_x, -real_y], dtype=np.float32)
+    pos = pos / 1000
+    return pos 
+
+
+def get_real_xyz(x, y, depth, w=0, h=0):
+    pos = get_real_xyz_base_on_camera(x, y, depth, w, h)
+    pos = pos + (0.0, 0.0, 0.6)
+    return pos
+
+
+def get_real_xyz(x, y, depth, w=0, h=0, rpy=(0, 0, 0)):
+    pos = get_real_xyz_base_on_camera(x, y, depth, w, h)
 
     rpy = np.array(rpy, dtype=np.float32) + (0.0, np.deg2rad(30), 0.0)
     roll, pitch, yaw = rpy
-    pos = pos / 1000 + (0.08, 0.00, 0.08)
+    pos = pos + (0.08, 0.015, 0.08)
     pos = rot_z(yaw) @ rot_y(pitch) @ rot_x(roll) @ pos
-    pos = pos + (0.0, 0.0, 0.64)
+
+    pos = pos + (0.0, 0.0, 0.6)
     return pos
+
 
 def rot_x(roll):
     c = np.cos(roll)
